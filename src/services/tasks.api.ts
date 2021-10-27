d722bc5ade6158e6fc702a0bfdfae55bdfd89c00
jheremy <jheremy808@gmail.com>
2021-10-26 23:02:08 -0400

    playlist service fixed

--- src/services/tasks.api.ts
+++ src/services/tasks.api.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const options: any = {
     method: 'GET',
     url: 'http://192.168.1.27:3000/api/playlists',
+    //url: 'http://192.168.1.27:3000/api/playlists',
 }
 
 export const getAllPlaylists = ()=>{
